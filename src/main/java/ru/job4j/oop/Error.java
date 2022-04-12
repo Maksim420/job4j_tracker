f205c423e30173fa610675cb51816f18f55779a0
Maksim420 <popovmv92@gmail.com>
2022-04-12 20:51:05 +0500

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,34 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void show() {
+        System.out.println("active " + this.active);
+        System.out.println("status " + this.status);
+        System.out.println("message " + this.message);
+    }
+
+    public static void main(String[] args) {
+        Error er1 = new Error();
+        Error er2 = new Error(true, 1, "Critical error");
+        Error er3 = new Error(false, 2, "Fatal error");
+        er1.show();
+        er2.show();
+        er3.show();
+    }
+
+}
