78b95626845b9e45025bb0a6a96d9467faebb7e5
Maksim420 <popovmv92@gmail.com>
2022-04-16 16:32:49 +0500

    2.4. Реализация класса StartUI. Вывод всех заявок [#500745]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -18,6 +18,16 @@ public class StartUI {
                 Item item = new Item(name);
                 tracker.add(item);
                 System.out.println("New item: " + item);
+            } else if (select == 1) {
+                System.out.println("=== Show all items ===");
+                Item[] items = tracker.findAll();
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("No items");
+                }
             } else if (select == 6) {
                 run = false;
             }
