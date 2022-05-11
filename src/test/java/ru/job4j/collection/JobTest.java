798a52c09a7a7c643c442a995c7a0e51bb474bf2
Maksim420 <popovmv92@gmail.com>
2022-05-11 11:04:15 +0500

    2. Комбинированный компаратор [#10036]

---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,67 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Comparator;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class JobTest {
+
+    @Test
+    public void whenComparatorAscByName() {
+        int rsl = new JobAscByName().compare(
+                new Job("First", 0),
+                new Job("First", 1)
+        );
+        assertThat(rsl, equalTo(0));
+    }
+
+    @Test
+    public void whenComparatorAscByPriority() {
+        int rsl = new JobAscByPriority().compare(
+                new Job("First", 0),
+                new Job("First", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorDescByName() {
+        int rsl = new JobDescByName().compare(
+                new Job("First", 0),
+                new Job("Second", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void whenComparatorDescByPriority() {
+        int rsl = new JobDescByPriority().compare(
+                new Job("First", 0),
+                new Job("Second", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void whenComparatorDescByNameAndPriority() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorAscByPriorityAndDescByName() {
+        Comparator<Job> cmpPriorityName = new JobAscByPriority().thenComparing(new JobDescByName());
+        int rsl = cmpPriorityName.compare(
+                new Job("First", 1),
+                new Job("Second", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+}
